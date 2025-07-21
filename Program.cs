4e383d1d9899cd85e6a12c2512f0115577771090
kekcoke <kevin.oane@yahoo.com>
2025-07-21 14:43:49 -0700

    Read input + basic validation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace OldPhonePad {
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Console NumPad Converter. \n. Enter numpad numbers to convert it to a character. \n Type 'exit' to quit.");
+            const string exitCommand = "exit";
+
+            while (true)
+            {
+                Console.Write("Enter numpad number: ");
+                string validPattern = @"^[0-9#*]+$";
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == exitCommand)
+                {
+                    Console.WriteLine("Exiting the program.");
+                    break;
+                }
+
+                else if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Input cannot be empty. Please enter a valid numpad number.");
+                    continue;
+                }
+
+                else if (!Regex.IsMatch(input, validPattern))
+                {
+                    Console.WriteLine("Invalid input. Please enter only numbers, '#' or '*'.");
+                    continue;
+                }
+            }
+        }
+    }
+}
+
