af5b57e507bfd6ef139799d051f4b52060738481
kekcoke <kevin.oane@yahoo.com>
2025-07-21 16:09:44 -0700

    Split str input + handling

--- Program.cs
+++ Program.cs
@@ -38,40 +38,65 @@ namespace OldPhonePad {
             }
         }
 
-        private static string ConvertNumpadInput(string input)
+        public static string ConvertNumpadInput(string input)
         {
+            char[] delimiters = { ' ', '\t', '#' };
+            string[] numbersArray = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            string result = string.Empty;
 
-            return $"{input}";
+            try
+            {
+                var numpadDictionary = NumpadDictionary();
+
+                foreach (string number in numbersArray)
+                {
+                    if (numpadDictionary.TryGetValue(number, out string character))
+                    {
+                        result += character;
+                    }
+                    else
+                    {
+                        return $"Invalid input: '{number}' is not a valid numpad number.";
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.Error.WriteLine($"An error occurred: {ex.Message}");
+                return $"An error occurred while processing the input.";
+            }
+
+            return result;
         }
 
-        private Dictionary<string, string> NumpadDictionary()
+        private static Dictionary<string, string> NumpadDictionary()
         {
             return new Dictionary<string, string>
             {
                 { "1", "1" },
-                { "2", "ABC" },
+                { "2", "A" },
                 { "22", "B" },
                 { "222", "C" },
-                { "3", "DEF" },
+                { "3", "D" },
                 { "33", "E" },
                 { "333", "F" },
-                { "4", "GHI" },
+                { "4", "G" },
                 { "44", "H" },
                 { "444", "I" },
-                { "5", "JKL" },
+                { "5", "J" },
                 { "55", "K" },
                 { "555", "L" },
-                { "6", "MNO" },
+                { "6", "M" },
                 { "66", "N" },
                 { "666", "O" },
-                { "7", "PQRS" },
+                { "7", "P" },
                 { "77", "Q" },
                 { "777", "R" },
                 { "7777", "S" },
-                { "8", "TUV" },
+                { "8", "T" },
                 { "88", "U" },
                 { "888", "V" },
-                { "9", "WXYZ" },
+                { "9", "W" },
                 { "99", "X" },
                 { "999", "Y" },
                 { "9999", "Z" },
